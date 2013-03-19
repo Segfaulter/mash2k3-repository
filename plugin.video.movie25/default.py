b85a3aea59f50c3f1c346c3ddfd58a092c377c3e
Mash2k3-Home <sheikh>
2013-03-19 01:08:41 -0400

    desi

--- plugin.video.movie25/default.py
+++ plugin.video.movie25/default.py
@@ -599,6 +599,7 @@ def LiveStreams():
             ok=dialog.ok('[B]Attention!!![/B]', 'Please be carefull in this section','may have content unsuitable for children','please report at XBMCHUB if XXX content found.')
         addDir('Livestation News','http://mobile.livestation.com/',116,"%s/art/livestation.png"%selfAddon.getAddonInfo("path"))
         addDir('iLive Streams','ilive',119,"%s/art/ilive.png"%selfAddon.getAddonInfo("path"))
+        addDir('Desi Streams','desi',129,"%s/art/ilive.png"%selfAddon.getAddonInfo("path"))
         addDir('Castalba Streams','castalgba',122,"%s/art/castalba.png"%selfAddon.getAddonInfo("path"))
         addDir('Misc. Music Streams','music',127,"%s/art/miscmusic.png"%selfAddon.getAddonInfo("path"))
 
@@ -2295,6 +2296,85 @@ def MUSICSTREAMS():
         addLink('Vevo TV','http://vevoplaylist-live.hls.adaptive.level3.net/vevo/ch1/appleman.m3u8',"%s/art/vevotv.jpg"%selfAddon.getAddonInfo("path"))
 
 
+def DESISTREAMS():
+        addDir('Sports','sports',130,"%s/art/desistream.png"%selfAddon.getAddonInfo("path"))
+        addDir('English Channels','english',130,"%s/art/desistream.png"%selfAddon.getAddonInfo("path"))
+        addDir('Indian Channels','indian',130,"%s/art/desistream.png"%selfAddon.getAddonInfo("path"))
+        addDir('Pakistani Channels','pakistani',130,"%s/art/desistream.png"%selfAddon.getAddonInfo("path"))
+        addDir('Bangladeshi Channels','bangladeshi',130,"%s/art/desistream.png"%selfAddon.getAddonInfo("path"))
+
+def DESISTREAMSList(murl):
+        link=OPENURL('http://www.desistreams.tv/')
+        link=link.replace('\r','').replace('\n','').replace('\t','').replace('&nbsp;','')
+        if murl=='sports':
+            match=re.compile('<font color="brown" size="4"><b>Sports Channels</b></font></center>(.+?)<center><font color="brown" size="4"><b>Indian Channels</b></font></center>').findall(link)
+            for entry in match:
+                match2=re.compile('<a href="(.+?)" Target=.+?><center><img src="(.+?)" width=".+?" height=".+?" alt="" /></a><br />(.+?)<').findall(entry)
+                for url,thumb,name in match2:
+                    url=url.replace('http://desistreams.tv/','')
+                    addDir(name,'http://www.desistreams.tv/'+url,131,'http://www.desistreams.tv/'+thumb)
+        elif murl=='english':
+            match=re.compile('<font color="brown" size="4"><b>English Channels</b></font></center>(.+?)<center><font color="brown" size="4"><b>Bangladeshi Channels</b></font></center>').findall(link)
+            for entry in match:
+                match2=re.compile('<a href="(.+?)" Target=.+?><center><img src="(.+?)" width=".+?" height=".+?" alt="" /></a><br />(.+?)<').findall(entry)
+                for url,thumb,name in match2:
+                    url=url.replace('http://desistreams.tv/','')
+                    addDir(name,'http://www.desistreams.tv/'+url,131,'http://www.desistreams.tv/'+thumb)
+        elif murl=='indian':
+            match=re.compile('<font color="brown" size="4"><b>Indian Channels</b></font></center>(.+?)<center><font color="brown" size="4"><b>Pakistani Channels</b></font></center>').findall(link)
+            for entry in match:
+                match2=re.compile('<a href="(.+?)" Target=.+?><center><img src="(.+?)" width=".+?" height=".+?" alt="" /></a><br />(.+?)<').findall(entry)
+                for url,thumb,name in match2:
+                    url=url.replace('http://desistreams.tv/','')
+                    addDir(name,'http://www.desistreams.tv/'+url,131,'http://www.desistreams.tv/'+thumb)
+        elif murl=='pakistani':
+            match=re.compile('<font color="brown" size="4"><b>Pakistani Channels</b></font></center>(.+?)<center><font color="brown" size="4"><b>English Channels</b></font></center>').findall(link)
+            for entry in match:
+                match2=re.compile('<a href="(.+?)" Target=.+?><center><img src="(.+?)" width=".+?" height=".+?" alt="" /></a><br />(.+?)<').findall(entry)
+                for url,thumb,name in match2:
+                    url=url.replace('http://desistreams.tv/','')
+                    addDir(name,'http://www.desistreams.tv/'+url,131,'http://www.desistreams.tv/'+thumb)
+        elif murl=='bangladeshi':
+            match=re.compile('<font color="brown" size="4"><b>Bangladeshi Channels</b></font></center>(.+?)</table>').findall(link)
+            for entry in match:
+                match2=re.compile('<a href="(.+?)" Target=.+?><center><img src="(.+?)" width=".+?" height=".+?" alt="" /></a><br />(.+?)<').findall(entry)
+                for url,thumb,name in match2:
+                    url=url.replace('http://desistreams.tv/','')
+                    addDir(name,'http://www.desistreams.tv/'+url,131,'http://www.desistreams.tv/'+thumb)
+
+def DESISTREAMSLink(mname,murl):
+        link=OPENURL(murl)
+        playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+        playlist.clear()
+        link=link.replace('\r','').replace('\n','').replace('\t','').replace('&nbsp;','')
+        match=re.compile('<script type=\'text/javascript\'>fid=\'(.+?)\'; v_width=(.+?); v_height=(.+?);</script><script type=\'text/javascript\' src=\'(.+?)\'>').findall(link)
+        for fid,wid,hei,streamer in match[0:1]:
+            continue
+        livecaster = re.compile("livecaster").findall(streamer)
+        if len(livecaster)>0:
+            pageUrl='http://www.livecaster.tv/embed.php?u='+fid+'&vw='+wid+'&vh='+hei
+            if mname=='Live Footy' or mname=='PTV Sports':
+                if fid=='ptvsportsss1':
+                    fid='onlineptvsports'
+                    stream_url ='rtmp://cdn.livecaster.tv/broadcast playpath='+fid+' swfUrl=http://www.livecaster.tv/player/player.swf pageUrl='+pageUrl
+                else:
+                    stream_url ='rtmp://cdn.livecaster.tv/broadcast playpath='+fid+'.stream swfUrl=http://www.livecaster.tv/player/player.swf pageUrl='+pageUrl
+            else:
+                if fid =='Geosuper':
+                    fid='13246540114814'
+                stream_url ='rtmp://cdn.livecaster.tv/stream playpath='+fid+' swfUrl=http://www.livecaster.tv/player/player.swf live=true timeout=15 pageUrl='+pageUrl
+            
+        ukcast = re.compile("ukcast").findall(streamer)
+        if len(ukcast)>0:
+            pageUrl='http://www.ukcast.tv/embed.php?u='+fid+'&vw='+wid+'&vh='+hei
+            stream_url ='rtmp://live.ukcast.tv/broadcast playpath='+fid+'.stream swfUrl=http://www.ukcast.tv/player/player.swf pageUrl='+pageUrl
+        listitem = xbmcgui.ListItem(mname)
+        playlist.add(stream_url,listitem)
+        xbmcPlayer = xbmc.Player()
+        xbmcPlayer.play(playlist)
+        addDir('','','','')
+
+        
 ###############################################################################################upupup##############################################################################        
 def Searchhistory():
         seapath=os.path.join(datapath,'Search')
@@ -6495,6 +6575,14 @@ elif mode==127:
         MUSICSTREAMS()
 elif mode==128:
         Clearhistory(url)
+
+elif mode==129:
+        DESISTREAMS()
+elif mode==130:
+        DESISTREAMSList(url)
+elif mode==131:
+        DESISTREAMSLink(name,url)
+
         
 elif mode==500:
         TVAll()        
